854a134d3c211509295e1daa300928ef3c8b2ee3
angel <1015770492@qq.com>
2020-05-11 18:29:39 +0800

    程序员B第一次提交

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("我是程序员B");//程序员B添加了注释；你好程序员A；你吃了吗？
         System.out.println("该吃晚饭了");
+        System.out.println("晚上吃什么");
     }
 }
