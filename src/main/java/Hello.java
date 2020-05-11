f9da631533602c9a83e7b4895b32b592bb5b820d
QiFeng <tenqifeng@163.com>
2020-05-11 18:30:17 +0800

    A来

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("我是程序员B");//程序员B添加了注释；你好程序员A；你吃了吗？
+        System.out.println("月季红来也");
         System.out.println("该吃晚饭了");
     }
 }
